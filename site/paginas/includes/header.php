9dc8a4fa1ee6315542f5cfc67d5b1994f14c295c
User <User@DESKTOP-4FVV4N6>
2021-02-22 12:03:46 -0300

    Alteração página artigos. Falta organizar o texto com a imagem.

--- site/paginas/includes/header.php
+++ site/paginas/includes/header.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
-        
+
         <meta charset="utf-8">
         <title>AdminLTE 3 | 404 Page not found</title>
 
         <!--Plugin bootstrap ofc-->
         <link rel="stylesheet" href="site/plugins/bootstrap/css/bootstrap.min.css" />
         <!--fim bootestrap ofc-->
-        
+
         <!-- Bootstrap ADM-->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
         <!-- Google Font: Source Sans Pro -->
@@ -18,11 +18,11 @@
         <!-- Theme style -->
         <link rel="stylesheet" href="painel/dist/css/adminlte.min.css">
 
-        <!--footer plugin blog-->
+
         <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css'>
         <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css'>      
-        <link rel="stylesheet" href="site/plugins/blog-footer-template/dist/style.css">
-        <!--fim p. footer-->    
+        <link rel="stylesheet" href="site/plugins/blog-footer-template/dist/style.css"
+   
     </head>
     <body>
 
--- site/paginas/paginaartigos.php
+++ site/paginas/paginaartigos.php
@@ -1,78 +1,95 @@
 
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>FinalPWmanha | Página de artigos</title>
 
-        <!-- Bootstrap -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-        <!-- Google Font: Source Sans Pro -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-        <!-- Font Awesome -->
-        <link rel="stylesheet" href="painel/plugins/fontawesome-free/css/all.min.css">
-        <!-- Theme style -->
-        <link rel="stylesheet" href="painel/dist/css/adminlte.min.css">
+<div class="container">
+    <div class="row">
+        <img src="../../painel/assets/img/logosenac" class="img-fluid m-auto w-25" alt="Imagem responsiva" >        
+    </div>
+    <div class="row m-auto text-center w-25">
+        <small class="text-justify"></small>
+    </div>
+    <div class="row">
+        <div class="badge badge-primary m-2 font-weight-bold">
+            Área de conhecimento
+        </div>
+    </div>
 
-        <!-- Google Font: Source Sans Pro -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-        <!-- Font Awesome -->
-        <link rel="stylesheet" href="painel/plugins/fontawesome-free/css/all.min.css">
-        <!-- Theme style -->
-        <link rel="stylesheet" href="painel/dist/css/adminlte.min.css">
-    </head>
-    <body>
+    <div class="card border-primary mt-2">
+        <div class="card-header text-bold">Curso</div>
+        <div class="card-body text-primary">
+            <small>Aguinácio, Manoel</small> <br />
+            <small>25/02/2021</small> <br />
+            <small>manoelag@gmail.com</small>
+        </div>
+    </div>
 
-        <div class="container">
-            <div class="row">
-                <img src="../../painel/assets/img/logosenac" class="img-fluid m-auto w-25" alt="Imagem responsiva" >        
+    <!--essa tag de vídio vai aparecer na minha página como um campo enorme vazio 
+    porem, quando o banco de dados analisar se existe ou não um vídeo para ser postado, 
+    ele vai por a row do texto no lugar certo. A teg precisa existir para o vídeo poder ser
+    adicionado, mas o layout vai depender do banco de dados
+    
+        <div class="row">
+            <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src="" allowfullscreen></iframe>
             </div>
-            <div class="row m-auto text-center w-25">
-                <small class="text-justify">Study Room Co-Working</small>
-            </div>
-            <div class="row">
-                <div class="alert alert-primary m-5">
-                    Área de conhecimento
-                </div>
-            </div>
+        </div>
+    
+    -->
 
-            <div class="card border-primary mb-3" style="max-width: 18rem;">
-                <div class="card-header">Título do Artigo</div>
-                <div class="card-body text-primary">
-                    <small>Nome da (o) autora:</small> <br />
-                    <small>Data de postagem:</small> <br />
-                    <small>E-mail para contato</small>
-                </div>
+    <p class="h1 text-center mb-5 mt-5">Título do artigo</p>
+    <div class="row">
 
-                <div class="row">
-                    <div class="card text-center">
-                        <div class="card-header">
-                            Destaque
-                        </div>
-                        <div class="card-body">
-                            <h5 class="card-title">Título especial</h5>
-                            <p class="card-text">Com suporte a texto embaixo, que funciona como uma introdução a um conteúdo adicional.</p>
-                            <a href="#" class="btn btn-primary">Visitar</a>
-                        </div>
-                        <div class="card-footer text-muted">
-                            2 dias atrás
-                        </div>
-                    </div>
+        <div class="col-8 ">
 
-                </div>
+            <p class="text-justify">
+                What is Lorem Ipsum?
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 
-            </div>
+                Why do we use it?
+                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
 
 
+                Where does it come from?
+                Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+
+                The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+
+                What is Lorem Ipsum?
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+                Why do we use it?
+                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+
+
+                Where does it come from?
+                Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+
+                The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+
+                What is Lorem Ipsum?
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+                Why do we use it?
+                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+
+            </p>
+
+        </div> 
+        <div class="col-4">            
+            <img class="img-fluid img-thumbnail mb-5" src="http://placehold.it/400x400" alt="Imagem de artigo">
+            <img class="img img-thumbnail mb-5" src="http://placehold.it/400x400" alt="Imagem de artigo">
+            <img class="img img-thumbnail mb-5" src="http://placehold.it/400x400" alt="Imagem de artigo">
+            
+        </div>
+
+    </div>
+</div>
 
 
 
-            <script src="painel/plugins/jquery/jquery.min.js"></script>
-            <!-- Bootstrap 4 -->
-            <script src="painel/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-            <!-- AdminLTE App -->
-            <script src="painel/dist/js/adminlte.min.js"></script>
-            <!-- AdminLTE for demo purposes -->
-            <script src="painel/dist/js/demo.js"></script><!-- /.control-sidebar -->
-    </body>
-</html>
+
+
+
+
+
+
+
